11001c8b5f56ad578c700ae2de4ccbf44ab248b9
MB13WIN10 <mihailbab13@yandex.ru>
2022-03-27 22:55:59 +0300

    hints added

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ const formBtnEl = document.getElementById('openModal');
 здесь использую его. Хоть и не большое кол-во элементов, но какая-никакая оптимизация :D*/
 const bodyEl = document.querySelector('body');
 const modalEl = document.getElementById('modal');
-const modalBodyEl = document.querySelector('.modal__body');
+const modalBodyEl = document.getElementById('modal-body');
 const closeModalBtnEl = document.getElementById('cancelBtn');
-const closeCrossEl = document.getElementById('closeCross')
+const closeCrossEl = document.getElementById('closeCross');
+const nameHintEl = document.getElementById('name-hint');
+const telHintEl = document.getElementById('tel-hint');
 
 formBtnEl.addEventListener('click', e => {
     bodyEl.classList.add('scroll-lock');
@@ -16,12 +18,12 @@ formBtnEl.addEventListener('click', e => {
 
 //Решил через делегирвание сделать + чтобы клик вне тоже закрывал
 modalEl.addEventListener('click', e => {
-    if(e.target === modalEl || e.target === closeModalBtnEl || e.target === closeCrossEl){
+    if (e.target === modalEl || e.target === closeModalBtnEl || e.target === closeCrossEl) {
         e.preventDefault();
         bodyEl.classList.remove('scroll-lock');
         modalEl.classList.remove('open')
     }
-    
+
 })
 
 
@@ -37,27 +39,32 @@ const errorEl = document.getElementById('error');
 
 
 //Вывод сделал раз за сеанс, для новой попытки - перезагрузка страницы
+
 formEl.addEventListener('submit', e => {
     e.preventDefault();
-    if(nameReg.test(nameInputEl.value)){
+    if (nameReg.test(nameInputEl.value)) {
         nameInputEl.classList.remove('invalid');
+        nameHintEl.classList.remove('visible');
     } else {
         nameInputEl.classList.add('invalid');
+        nameHintEl.classList.add('visible');
     }
-    if(telReg.test(telInputEl.value)){
+    if (telReg.test(telInputEl.value)) {
         telInputEl.classList.remove('invalid');
+        telHintEl.classList.remove('visible');
     } else {
         telInputEl.classList.add('invalid');
+        telHintEl.classList.add('visible');
     }
-    if(nameReg.test(nameInputEl.value) && telReg.test(telInputEl.value)){
+    if (nameReg.test(nameInputEl.value) && telReg.test(telInputEl.value)) {
         modalBodyEl.classList.add('invisible');
         spinerEl.classList.add('visible');
         return fetch(`https://jsonplaceholder.typicode.com/todos`)
-        //Если успешно -> перебираем все элементы, подходящие критериям запроса
+            //Если успешно -> перебираем все элементы, подходящие критериям запроса
             .then(result => result.json())
             .then(result => {
                 result.forEach(el => {
-                    if(el.userId === 5 && el.completed === false){
+                    if (el.userId === 5 && el.completed === false) {
                         console.log(el)
                         tableEl.insertAdjacentHTML('beforeend', `
                         <tr>
@@ -76,16 +83,18 @@ formEl.addEventListener('submit', e => {
                         </tr>    
                         `)
                     }
+                    //Скрытие загрузки и показ рещультата решил сделать здесь, а не через finally,
+                    //т.к. уже после цикла все данные вставлены 
                     spinerEl.classList.remove('visible')
                     tableEl.classList.add('open')
                 });
-                })
+            })
             //Если не получилось - будет окно с сообщением и так же надо будет перезагрузить страницу
             .catch(error => {
                 console.log(error);
                 errorEl.classList.add('visible');
                 spinerEl.classList.remove('visible');
-                
+
             });
-    } 
+    }
 })
